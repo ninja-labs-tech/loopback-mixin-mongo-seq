0e5c81128c6b9257de5458b97a02b60c31aacfb9
Mohammed Essehemy <mohammedessehemy@gmail.com>
2018-10-13 17:54:27 +0200

    initialize repo

---
+++ README.md
@@ -0,0 +1,47 @@
+### MONGO-SEQ ###
+
+[loopback v3](https://loopback.io/) mixin to add support for sequential property.
+
+### usage ###
+1.
+```
+npm install loopback-mixin-mongo-seq
+```
+2. update `mixins` property in `server/model-config.json`:
+
+```json
+{
+  "_meta": {
+    "sources": [
+      "loopback/common/models",
+      "loopback/server/models",
+      "../common/models",
+      "./models"
+    ],
+    "mixins": [
+      "loopback/common/mixins",
+      "../common/mixins",
+      "../node_modules/loopback-mixin-mongo-seq/lib"
+    ]
+  }
+}
+```
+3. add mixins property to the required model.
+
+```json
+  "mixins": {
+     "Seq" : {
+       "counterModelName": "Counter",
+       "counterModelDataSource": "MongoDS",
+       "seqPropertyName": "ID",
+       "step": 1
+     }
+   }
+```
+
+
+### DEBUG MODE ###
+
+```
+  DEBUG='loopback:mixin:mongo-seq'
+```
---
+++ helpers/initializeCounterModel.js
@@ -0,0 +1,62 @@
+const debug = require('debug')('loopback:mixin:mongo-seq')
+
+module.exports = (app, opts) => {
+  debug(`initializing counter model with options ${JSON.stingify(opts)}`);
+
+  const counterModel = app.loopback.createModel({
+    name: opts.counterModelName,
+    base: 'PersistedModel',
+    strict: 'filter',
+    options: {
+      validateUpsert: true,
+    },
+    properties: {
+      nextVal: {
+        type: 'number',
+      },
+    },
+    validations: [],
+    relations: {},
+    acls: [],
+    methods: {},
+  });
+  counterModel.createSeq = async ({ name, initialVal = 1 }) => {
+    const counterCollection = counterModel.getDataSource().connector.collection(counterModel.modelName);
+    const { value: doc } = await counterCollection.findOneAndUpdate({ _id: name }, {
+      $setOnInsert: {
+        _id: name,
+      },
+      $set: {
+        nextVal: initialVal,
+      },
+    }, {
+      upsert: true,
+      returnOriginal: false,
+    });
+    return doc && doc.nextVal;
+  };
+
+  counterModel.incrementSeq = async ({ name, step = 1 }) => {
+    const counterCollection = counterModel.getDataSource().connector.collection(counterModel.modelName);
+    const { value: doc } = await counterCollection.findOneAndUpdate({ _id: name }, {
+      $inc: {
+        nextVal: step,
+      },
+    }, {
+      returnOriginal: true,
+    });
+    return doc && doc.nextVal;
+  };
+
+  counterModel.getSeqVal = ({ name }) => counterModel.findOne({ where: { _id: name } });
+  app.model(counterModel, {
+    dataSource: opts.counterModelDataSource,
+    public: false,
+  });
+
+  counterModel.initializeSeq = async (Model) => {
+    const highestRecord = await Model.findOne({ order: 'ID DESC', fields: ['ID'] });
+    const initialVal = ((highestRecord && highestRecord[opts.seqPropertyName]) + 1) || 1;
+    await counterModel.createSeq({ name: Model.modelName, initialVal });
+  };
+};
