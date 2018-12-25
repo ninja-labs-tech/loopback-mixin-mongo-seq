0bb12d6766124525f132352407111d6aeaf16ce4
Mohamed Hassan <mohamed.hassan@fixedmea.com>
2018-12-25 17:44:17 +0200

    resolved issue of counter model not initialized

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const initilaizeCounterModel = require('./lib/initializeCounterModel');
 const seq = require('./lib/seq');
 
 const defaultCounterModelOpts = {
@@ -7,6 +6,5 @@ const defaultCounterModelOpts = {
 };
 module.exports = function loopbackMixin(app, options) {
   const CounteModelOpts = Object.assign(defaultCounterModelOpts, options);
-  const CounterModel = initilaizeCounterModel(app, CounteModelOpts);
-  app.loopback.modelBuilder.mixins.define('Seq', seq(CounterModel));
+  app.loopback.modelBuilder.mixins.define('Seq', seq(app, CounteModelOpts));
 };
